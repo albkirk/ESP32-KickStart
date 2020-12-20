d61bf96bc2731d6b23048bbaa82851b34bf2550a
Nuno Albuquerque <albkirk@gmail.com>
2020-12-20 01:30:43 +0000

    v12.15 - adding ULP action (Cleaning project)

--- 20200121 ESP32 Web Cam/esp32-cam-webserver-1.0/lib/project/README.md
+++
@@ -1,3 +0,0 @@
-these are the files where you should add all MQTT and STORAGE Project related items/actions
-  - customqtt.h
-  - custostore.h
--- 20200121 ESP32 Web Cam/esp32-cam-webserver-1.0/lib/project/custommqtt.h
+++
@@ -1,5 +0,0 @@
-// Function to insert customized MQTT Configuration actions
-
-void  mqtt_custom(String reqparam, String reqvalue, StaticJsonDocument<200> data) {
-  if ( reqparam == "LED2") { config.LED = data["value"];storage_write(); }
-}
